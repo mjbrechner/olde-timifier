806cf18a19778cbb35621e57ab6fbd4a3f8cea1c
MJ Brechner <mjbrechner@gmail.com>
2023-01-02 14:22:19 -0500

    regex updates for long s

--- javascript/index.js
+++ javascript/index.js
@@ -5,9 +5,27 @@ function convertText() {
 let originalText = document.getElementById("text-input").value;
 
 let textOutputBox = document.getElementById("text-output");
-let newText = originalText.replace(/t/g, "þ").replace(/T/g, "Þ").replace(/s(?!\s)/g, "ſ");
 
-// .replace(/ſ+\s, "s")
+// Here is where the conversion happens, divided into separate steps to explain the process.
+
+// Add THORN, which replaces TH.
+let newText = originalText.replace(/th/g, "þ").replace(/TH/g, "Þ").replace(/Th/g, "Þ");
+
+// Add LONG S, which replaces LOWERCASE S, to everything except the ends of words (and the end of the string).
+newText = newText.replace(/s(?![.!?\s])(?!$)/g, "ſ");
+
+// In the cases of a double LONG S, change the second one back to a SHORT S.
+newText = newText.replace(/ſſ/g, "ſs");
+
+// If LONG S comes before APOSTROPHE, change back to SHORT S.
+newText = newText.replace(/ſ(?=['`’])/g, "s");
+
+// If LONG S comes after F, change back to SHORT S.
+newText = newText.replace(/(?<=f)ſ/g, "s");
+
+// If LONG S comes before F, change back to SHORT S.
+newText = newText.replace(/ſ(?=f)/g, "s");
+
 textOutputBox.textContent = newText;
 
 
