9dc817597f7d29fc5da4705005a4a0bf179ef9dd
MJ Brechner <mjbrechner@gmail.com>
2023-01-03 17:11:11 -0500

    converts from modern to olden, preserving capitalization

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,21 @@
 'use strict';
 
-
-function convertText() {
-let originalText = document.getElementById("text-input").value;
-
+let originalText;
 let textOutputBox = document.getElementById("text-output");
+let newText;
+const isUpperCase = str => str === str.toUpperCase();
+
+function convertTextOlde() {
+originalText = document.getElementById("text-input").value;
+newText = originalText;
 
 // Here is where the conversion happens, divided into separate steps to explain the process.
 
 // Add THORN, which replaces TH.
-let newText = originalText.replace(/th/g, "þ").replace(/TH/g, "Þ").replace(/Th/g, "Þ");
+newText = newText.replace(/th/g, "þ").replace(/TH/g, "Þ").replace(/Th/g, "Þ");
 
 // Add LONG S, which replaces LOWERCASE S, to everything except the ends of words (and the end of the string).
-newText = newText.replace(/s(?![.!?\s])(?!$)/g, "ſ");
+newText = newText.replace(/s(?![.!?,;:\s])(?!$)/g, "ſ");
 
 // In the cases of a double LONG S, change the second one back to a SHORT S.
 newText = newText.replace(/ſſ/g, "ſs");
@@ -27,6 +30,28 @@ newText = newText.replace(/(?<=f)ſ/g, "s");
 newText = newText.replace(/ſ(?=f)/g, "s");
 
 textOutputBox.textContent = newText;
-
-
 }
+
+function convertTextModern() {
+originalText = document.getElementById("text-input").value;
+newText = originalText;
+
+// Here is where the conversion happens, divided into separate steps to explain the process.
+
+// Change THORN to TH.
+if (isUpperCase(newText) === true) {
+    // The text is ALL CAPS. THORN should convert to UPPERCASE T & UPPERCASE H.
+    newText = newText.replace(/þ/g, "th").replace(/Þ/g, "TH");
+} else {
+    // The text is not ALL CAPS. THORN should convert to UPPERCASE T &  LOWERCASE H.
+    newText = newText.replace(/þ/g, "th").replace(/Þ/g, "Th");
+}
+
+// Change LONG S to SHORT S.
+// No need to check for ALL CAPS, since LONG S is inherently a LOWERCASE letter.
+newText = newText.replace(/ſ/g, "s");
+
+textOutputBox.textContent = newText;
+}
+
+
